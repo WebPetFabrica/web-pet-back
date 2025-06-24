c190160a2dfa6afa276a36ff2ed1773e5518277c
Gabriel Felipe Guarnieri <gfguarnieri@gmail.com>
2025-06-24 20:17:06 -0300

    chore: Aplica sugestões de qualidade (migração, testes, indexes)

---
+++ src/test/java/br/edu/utfpr/alunos/webpet/controllers/UserControllerTest.java
@@ -0,0 +1,26 @@
+package br.edu.utfpr.alunos.webpet.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username="test@user.com", roles={"USER"})
+    void shouldReturnUserProfileForAuthenticatedUser() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk());
+    }
+}
